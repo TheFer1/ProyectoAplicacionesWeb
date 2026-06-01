0c9fa048c0828eaabd72728193644450dd2c76e3
Lizeth Hernandez <lizeth.hernandez@epn.edu.ec>
2026-05-31 20:30:15 -0500

    Mejora del score

--- src/ui/HUD.js
+++ src/ui/HUD.js
@@ -4,12 +4,21 @@ import StorageManager from '../managers/StorageManager.js';
 export default class HUD {
     constructor(scene) {
         this.scene = scene;
-        this.scoreText = scene.add.text(10, 10, '', { fontSize: '20px', fill: '#000' }).setScrollFactor(0);
-        this.livesText = scene.add.text(10, 40, '', { fontSize: '20px', fill: '#000' }).setScrollFactor(0);
-        this.levelText = scene.add.text(400, 10, '', { fontSize: '20px', fill: '#000' }).setOrigin(0.5, 0).setScrollFactor(0);
-        this.highScoreText = scene.add.text(790, 10, '', { fontSize: '20px', fill: '#000' }).setOrigin(1, 0).setScrollFactor(0);
-        
+        const { width } = scene.scale;
+
+        this.scoreText = scene.add.text(20, 20, '', { fontSize: '20px', fill: '#030303' }).setScrollFactor(0);
+        this.livesText = scene.add.text(20, 50, '', { fontSize: '20px', fill: 's#1d1b1b' }).setScrollFactor(0);
+        this.levelText = scene.add.text(width / 2, 20, '', { fontSize: '20px', fill: '#141212' }).setOrigin(0.5, 0).setScrollFactor(0);
+        this.highScoreText = scene.add.text(width - 20, 20, '', { fontSize: '20px', fill: '#0a0909' }).setOrigin(1, 0).setScrollFactor(0);
+
+        scene.scale.on('resize', this.resize, this);
         this.updateHUD();
+    }
+
+    resize(gameSize) {
+        const width = gameSize?.width || this.scene.scale.width;
+        this.levelText.setX(width / 2);
+        this.highScoreText.setX(width - 20);
     }
 
     updateHUD() {
